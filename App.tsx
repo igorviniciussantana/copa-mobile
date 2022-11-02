bf236541fd998910bf437fc0a84400f0548da7ce
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2022-11-02 11:09:36 -0400

    feat: import assets

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Text</Text>
+      <Text>Atualizado com sucesso!</Text>
       <StatusBar style="auto" />
     </View>
   );
