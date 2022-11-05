20a0d704cb688616a491cf572f029ebfeec18fe5
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2022-11-05 13:34:44 -0400

    feat: import components

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { NativeBaseProvider, StatusBar } from "native-base"
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold} from '@expo-google-fonts/roboto'
 
 import { Loading } from './src/components/Loading';
-import { SignIn } from './src/screens/SignIn';
+import { New } from './src/screens/New';
 
 import { THEME } from './src/styles/theme'
 import {  AuthContextProvider } from "./src/contexts/AuthContext";
@@ -22,7 +22,7 @@ export default function App() {
       translucent
       />
 
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <New /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
