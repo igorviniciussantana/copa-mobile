74599962352a0638d58aba932a9c7933fa6277ba
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2022-11-05 10:53:43 -0400

    feat: complete signIn screen

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,8 @@
-import { Center, Text } from "native-base";
+import { Center, Text, Icon } from "native-base";
+import { Fontisto } from '@expo/vector-icons'
+
+import Logo from "../assets/logo.svg";
+import { Button } from "../components/Button";
 
 export function SignIn() {
   return (
@@ -7,10 +11,19 @@ export function SignIn() {
       bgColor="gray.900"
       alignItems="center"
       justifyContent="center"
+      p={7}
     >
-      <Text color="white" fontSize={24} fontFamily='heading'>
-       SignIn
-      </Text>
+      <Logo width={212} height={40} />
+      <Button
+      type='SECONDARY'
+      title="ENTRAR COM O GOOGLE"
+      leftIcon={ <Icon as={Fontisto} name="google" color="white" size="md" />}
+      mt={12}
+      />
+
+      <Text color='white' textAlign="center" mt={4}>
+        Não utilizamos nenhuma informação além {'\n'} do seu e-mail para ccriação de sua conta.
+        </Text>
     </Center>
   );
 }
