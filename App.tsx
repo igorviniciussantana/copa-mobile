1e51c8553202080d2b5f696d5c7c0b786ec3c921
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2022-11-05 14:27:31 -0400

    feat: create find screen

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { NativeBaseProvider, StatusBar } from "native-base"
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold} from '@expo-google-fonts/roboto'
 
 import { Loading } from './src/components/Loading';
-import { New } from './src/screens/New';
+import { Find } from './src/screens/Find';
 
 import { THEME } from './src/styles/theme'
 import {  AuthContextProvider } from "./src/contexts/AuthContext";
@@ -22,7 +22,7 @@ export default function App() {
       translucent
       />
 
-      {fontsLoaded ? <New /> : <Loading />}
+      {fontsLoaded ? <Find /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
--- src/screens/Find.tsx
+++ src/screens/Find.tsx
@@ -6,32 +6,27 @@ import { Button } from "../components/Button";
 
 import Logo from "../assets/logo.svg";
 
-export function New() {
+export function Find() {
   return (
     <VStack flex={1} bgColor="gray.900">
-      <Header title="Criar Novo Bolão" />
+      <Header title="Buscar por código" showBackButton/>
 
       <VStack mt={8} mx={5} alignItems="center">
-        <Logo />
 
         <Heading
           fontFamily="heading"
           color="white"
           fontSize="xl"
-          my={8}
+          mb={8}
           textAlign="center"
         >
-          Crie seu próprio bolão da copa {'\n'} e compartilhe entre amigos!
+          Encontre um bolão através {'\n'}
+          de seu código único
         </Heading>
 
-        <Input mb={2} placeholder="Qual nome do seu bolão?" />
+        <Input mb={2} placeholder="Qual o código do bolão?" />
 
-        <Button title="CRIAR MEU BOLÃO" />
-
-        <Text color="gray.200" fontSize='sm' textAlign='center' px={10} mt={4}>
-          Após criar seu bolão, você receberá um código único que poderá usar
-          para convidar outras pessoas
-        </Text>
+        <Button title="BUSCAR BOLÃO" />
       </VStack>
     </VStack>
   );
